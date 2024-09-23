19b577c15658a4e813e776df7617e8b5a8c9f6fd
llRedXD <gh.o.neira@hotmail.com>
2024-09-23 17:07:15 -0300

    Atualizar configurações de idioma e fuso horário para pt-br e America/Sao_Paulo respectivamente

--- project/settings.py
+++ project/settings.py
@@ -134,11 +134,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = "pt-br"
 
 USE_I18N = True
+
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_TZ = True
 
