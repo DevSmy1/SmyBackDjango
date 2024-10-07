3e5cee240a345272da761e2dd15834cadcab4b57
llRedXD <gh.o.neira@hotmail.com>
2024-10-07 15:05:17 -0300

    feat: Atualizar código de importação de dados do arquivo Excel
    Atualiza o código de importação de dados do arquivo Excel no módulo `importacao.py`. Essa atualização inclui a adição do campo `nro_empresa` aos modelos `SetrabCargoRel`, `SetrabSetorRel` e `SetrabFuncaoRel, a formatação do CPF, a adição de tratamento de exceções e a inclusão de informações adicionais nos dados de colaboradores. Essas alterações são necessárias para melhorar a sincronização e integração dos dados com o site da SGG.

--- project/setrab/services/importacao.py
+++ project/setrab/services/importacao.py
@@ -1,5 +1,5 @@
 from datetime import date
-from project.controle_uni.models import TsmyEuColaboradores  # noqa: E402
+from project.controle_uni.models import TsmyEuCargos, TsmyEuColaboradores  # noqa: E402
 from project.setrab.models import SetrabCargoRel, SetrabEmpresaRel, SetrabSetorRel  # noqa: E402
 
 import pandas as pd
@@ -22,19 +22,20 @@ def admissao(arquivo: str, data: date):
             try:
                 empresa = SetrabEmpresaRel.objects.get(id_empresa=df.iloc[linha][1])
 
+                cpf = str(df.iloc[linha][0])
+                cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
                 cargo = SetrabCargoRel.objects.filter(
                     id_cargo=df.iloc[linha][7], nro_empresa=df.iloc[linha][1]
                 ).first()
                 if not cargo:
-                    continue
+                    raise Exception("Cargo não encontrado")
                 setor = SetrabSetorRel.objects.filter(
                     id_setor=str(df.iloc[linha][8]), nro_empresa=df.iloc[linha][1]
                 ).first()
                 if not setor:
-                    continue
+                    raise Exception("Setor não encontrado")
 
-                cpf = str(df.iloc[linha][0])
-                cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
                 colab = {
                     "id_empresa": empresa.id_empresa_setrab,
                     "id_funcionario": cpf,
@@ -45,11 +46,26 @@ def admissao(arquivo: str, data: date):
                     "id_cargo": cargo.id_cargo_setrab,
                     "id_setor": setor.id_setor_setrab,
                     "possui_vinculo": "S",
+                    "id_empresa_rh": empresa.id_empresa,
                 }
                 dados.append(colab)
 
             except Exception as e:
                 print(e)
+                colab = {
+                    "id_empresa": empresa.id_empresa_setrab,
+                    "id_funcionario": cpf,
+                    "nome": df.iloc[linha][3],
+                    "data_admissao": df.iloc[linha][4].strftime("%Y-%m-%d"),
+                    "data_nascimento": df.iloc[linha][5].strftime("%Y-%m-%d"),
+                    "sexo": df.iloc[linha][6][0],
+                    "id_cargo": 0,
+                    "id_setor": 0,
+                    "possui_vinculo": "S",
+                    "id_empresa_rh": empresa.id_empresa,
+                    "erro_sistema": str(e),
+                }
+                dados.append(colab)
         return dados
     except Exception as e:
         raise e
@@ -79,10 +95,21 @@ def demissao(arquivo: str, data: date):
                     "id_empresa": empresa.id_empresa_setrab,
                     "id_funcionario": cpf,
                     "data_demissao": df.iloc[linha][2].strftime("%Y-%m-%d"),
+                    "nome": colab.nome,
+                    "id_empresa_rh": empresa.id_empresa,
                 }
                 dados.append(demissao)
             except Exception as e:
                 print(e)
+                demissao = {
+                    "id_empresa": empresa.id_empresa_setrab,
+                    "id_funcionario": cpf,
+                    "data_demissao": df.iloc[linha][2].strftime("%Y-%m-%d"),
+                    "nome": colab.nome,
+                    "id_empresa_rh": empresa.id_empresa,
+                    "erro_sistema": str(e),
+                }
+                dados.append(demissao)
         return dados
     except Exception as e:
         raise e
@@ -98,6 +125,11 @@ def mudanca_funcao(arquivo: str, data: date):
         ]
         for linha in range(1, len(df)):
             try:
+                colab = TsmyEuColaboradores.objects.get(matricula=df.iloc[linha][1])
+
+                cpf = str(df.iloc[linha][0])
+                cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
                 empresa = SetrabEmpresaRel.objects.get(
                     id_empresa=int(df.iloc[linha][2])
                 )
@@ -105,26 +137,98 @@ def mudanca_funcao(arquivo: str, data: date):
                     id_cargo=df.iloc[linha][3], nro_empresa=empresa.id_empresa
                 ).first()
                 if not cargo:
-                    continue
+                    raise Exception("Cargo não encontrado")
 
                 setor = SetrabSetorRel.objects.filter(
                     id_setor=str(df.iloc[linha][4]), nro_empresa=empresa.id_empresa
                 ).first()
                 if not setor:
-                    continue
+                    raise Exception("Setor não encontrado")
 
-                cpf = str(df.iloc[linha][0])
-                cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
                 mudanca_funcao = {
                     "id_funcionario": cpf,
                     "id_empresa": empresa.id_empresa_setrab,
                     "id_cargo": cargo.id_cargo_setrab,
                     "id_setor": setor.id_setor_setrab,
                     "data_mud_func": df.iloc[linha][5].strftime("%Y-%m-%d"),
+                    "nome": colab.nome,
+                    "id_empresa_rh": empresa.id_empresa,
                 }
                 dados.append(mudanca_funcao)
             except Exception as e:
+                muda_funcao = {
+                    "id_funcionario": cpf,
+                    "id_empresa": empresa.id_empresa_setrab,
+                    "id_cargo": 0,
+                    "id_setor": 0,
+                    "data_mud_func": df.iloc[linha][5].strftime("%Y-%m-%d"),
+                    "nome": colab.nome,
+                    "id_empresa_rh": empresa.id_empresa,
+                    "erro_sistema": str(e),
+                }
+                dados.append(muda_funcao)
+        return dados
+    except Exception as e:
+        raise e
+
+
+def transferencia(arquivo: str, data: date):
+    try:
+        dados = []
+        df = pd.read_excel(arquivo)
+        df = df[
+            (df.iloc[:, 6].dt.month == pd.to_datetime(data).month)
+            & (df.iloc[:, 6].dt.year == pd.to_datetime(data).year)
+        ]
+        for linha in range(1, len(df)):
+            try:
+                empresa_atual = SetrabEmpresaRel.objects.get(
+                    id_empresa=int(df.iloc[linha][2][:2])
+                )
+                empresa_destino = SetrabEmpresaRel.objects.get(
+                    id_empresa=int(df.iloc[linha][2][:2])
+                )
+                colab = TsmyEuColaboradores.objects.get(matricula=df.iloc[linha][0])
+                cpf = str(colab.cpf)
+                cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
+                cargo = SetrabCargoRel.objects.filter(
+                    id_cargo=colab.cod_funcao, nro_empresa=empresa_atual.id_empresa
+                ).first()
+                if not cargo:
+                    raise Exception("Cargo não encontrado")
+
+                setor = SetrabSetorRel.objects.filter(
+                    id_setor=str(df.iloc[linha][5][:12]),
+                ).first()
+                if not setor:
+                    raise Exception("Setor não encontrado")
+
+                transferencia = {
+                    "id_funcionario": cpf,
+                    "id_empresa": empresa_atual.id_empresa_setrab,
+                    "id_empresa_destino": empresa_destino.id_empresa_setrab,
+                    "id_cargo": cargo.id_cargo_setrab,
+                    "id_setor": setor.id_setor_setrab,
+                    "data_transferencia": df.iloc[linha][6].strftime("%Y-%m-%d"),
+                    "nome": colab.nome,
+                    "id_empresa_rh": empresa_atual.id_empresa,
+                }
+                dados.append(transferencia)
+            except Exception as e:
                 print(e)
+                transferencia = {
+                    "id_funcionario": cpf,
+                    "id_empresa": empresa_atual.id_empresa_setrab,
+                    "id_empresa_destino": empresa_destino.id_empresa_setrab,
+                    "id_cargo": 0,
+                    "id_setor": 0,
+                    "data_transferencia": df.iloc[linha][6].strftime("%Y-%m-%d"),
+                    "nome": colab.nome,
+                    "id_empresa_rh": empresa_atual.id_empresa,
+                    "erro_sistema": str(e),
+                }
+                dados.append(transferencia)
         return dados
     except Exception as e:
         raise e
